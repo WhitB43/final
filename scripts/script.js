eb85955e59c6c389fcd4ce7f686a1b2b83edc052
whitbrewer <fwrdaerial@gmail.com>
2024-12-02 20:22:46 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,53 +5,61 @@ var order = {
     vegetables: 'Lettuce, Onion, Black Olive, Green Olive'
 };
 
+var newOrder = {
+    sS: '',
+    bread: '',
+    cheese: '',
+    vegetables: ''
+};
+
 function send() {
     
     if($('#fL').is(':checked')) {
-        order.sS = 'Footlong';
+        newOrder.sS = 'Footlong';
     }
     if($('#6i').is(':checked')) {
-        order.sS = '6 Inch';
+        newOrder.sS = '6 Inch';
     }
 
-    order.bread = $('#bread').find(":selected").val();
+    newOrder.bread = $('#bread').find(":selected").val();
 
-   order.cheese = $('#cheese').find(":selected").val();
+    newOrder.cheese = $('#cheese').find(":selected").val();
 
     if($('#checkL').is(':checked')){
-        order.vegetables = order.vegetables + $('checkL').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkL').val() + ", ";
     }
     if($('#checkT').is(':checked')){
-        order.vegetables = order.vegetables + $('checkT').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkT').val() + ", ";
     }
     if($('#checkP').is(':checked')){
-        order.vegetables = order.vegetables + $('checkP').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkP').val() + ", ";
     }
     if($('#checkO').is(':checked')){
-        order.vegetables = order.vegetables + $('checkO').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkO').val() + ", ";
     }
     if($('#checkM').is(':checked')){
-        order.vegetables = order.vegetables + $('checkM').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkM').val() + ", ";
     }
     if($('#checkBP').is(':checked')){
-        order.vegetables = order.vegetables + $('checkBP').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkBP').val() + ", ";
     }
     if($('#checkJ').is(':checked')){
-        order.vegetables = order.vegetables + $('checkL').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkL').val() + ", ";
     }
     if($('#checkBO').is(':checked')){
-        order.vegetables = order.vegetables + $('checkBO').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkBO').val() + ", ";
     }
     if($('#checkGO').is(':checked')){
-        order.vegetables = order.vegetables + $('checkGO').val() + ", ";
+        newOrder.vegetables = newOrder.vegetables + $('checkGO').val() + ", ";
     }
 };
 
 document.getElementById('order').addEventListener('submit', (e) => {
   e.preventDefault();
   send();
-  console.log(JSON.stringify(order));
-  document.getElementById('sent').innerText = JSON.stringify(order, null, 2);
+  console.log(JSON.stringify(newOrder));
+  document.getElementById('sent').innerText = JSON.stringify(newOrder, null, 2);
+  order = newOrder;
 });
 
 document.getElementById('load').addEventListener('click', () => {
